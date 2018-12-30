7cf6159e0d8b1dea3aba3cb8437bd05bf65cb847
Jonathan Sexton <joncsexton@.com>
2018-12-30 14:44:23 -0600

    Finally got the request to work properly

--- js/public.js
+++ js/public.js
@@ -3,17 +3,29 @@ let userName = document.querySelector("#userName");
 let user;
 
 // fetch GitHub user data
-const userData = fetch("https://api.github.com/users/js-goose")
-  .then((response) => response.json())
-  .then((response) => {
-    user = response;
-    // userImg.innerHTML = `User Image: <img src=${user.avatar_url} height=100 width = 100 alt='user image of js-goose'>`
-  })
-  .catch((error) => console.log("There was an error fetching the data: " + error));
+const getUser = () => {
+  fetch("https://api.github.com/users/js-goose")
+    .then((response) => {
+      if (response.status !== 200) {
+        return Promise.reject({
+          status: response.status,
+          statusText: response.statusText,
+        });
+      } else {
+      return response.json()
+      }
+    })
+    .then((user) => {
+      console.log(user);
+    })
+    .catch((error) => {
+      if (error.status === 404) {
+        let body = document.querySelector('body');
+        body.innerHTML = `Oh no! There was an error of: ${error}`
+        console.error("There was an error fetching the data: " + error)
+      }
+    });
+};
 
-setTimeout(() => {
-  console.log(user.login);
-  userImg.innerHTML = `User Image: <img src=${
-    user.avatar_url
-  } height=100 width = 100 alt='user image of js-goose'>`;
-}, 1500);
+getUser();
+
