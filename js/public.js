27549816d7cf77c370ba7f6ebaeb66ce5e2ad72a
Jonathan Sexton <joncsexton@.com>
2018-12-30 14:03:14 -0600

    still trying to get the request to work propertly

--- js/public.js
+++ js/public.js
@@ -1,4 +1,5 @@
-let userImg = document.querySelector("#user-img");
+let userImg = document.querySelector("#userImg");
+let userName = document.querySelector("#userName");
 let user;
 
 // fetch GitHub user data
@@ -6,13 +7,13 @@ const userData = fetch("https://api.github.com/users/js-goose")
   .then((response) => response.json())
   .then((response) => {
     user = response;
+    // userImg.innerHTML = `User Image: <img src=${user.avatar_url} height=100 width = 100 alt='user image of js-goose'>`
   })
-  .catch((error) =>
-    console.log("There was an error fetching the data: " + error)
-  );
+  .catch((error) => console.log("There was an error fetching the data: " + error));
 
-
-setTimeout(()=> {
-  console.log(user.login)
-  userImg.innerHTML = `<img src=${user.avatar_url} height=100 width = 100 alt='user image of js-goose'>`
-}, 500);
+setTimeout(() => {
+  console.log(user.login);
+  userImg.innerHTML = `User Image: <img src=${
+    user.avatar_url
+  } height=100 width = 100 alt='user image of js-goose'>`;
+}, 1500);
