1616085e028089db370e3f5448a41fe18753b578
Jonathan Sexton <joncsexton@.com>
2018-12-30 13:21:36 -0600

    updated api request for github info

--- js/public.js
+++ js/public.js
@@ -1,14 +1,18 @@
 let userImg = document.querySelector("#user-img");
+let user;
 
 // fetch GitHub user data
 const userData = fetch("https://api.github.com/users/js-goose")
   .then((response) => response.json())
   .then((response) => {
-    userImg.innerHTML = `<img src=${response.avatar_url} height=100 width = 100 alt='user image of js-goose'>`
-    console.log(response);
+    user = response;
   })
   .catch((error) =>
     console.log("There was an error fetching the data: " + error)
   );
 
 
+setTimeout(()=> {
+  console.log(user.login)
+  userImg.innerHTML = `<img src=${user.avatar_url} height=100 width = 100 alt='user image of js-goose'>`
+}, 500);
