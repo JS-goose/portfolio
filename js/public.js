ef597ab478cacb2cd37729dcbcd687ab3493f129
Jonathan Sexton <joncsexton@gmail.com>
2018-12-29 18:13:56 -0600

    established code to pull in GitHub profile info

--- js/public.js
+++ js/public.js
@@ -1,4 +1,14 @@
+let userSection = document.querySelector(".user-data");
+
 // fetch GitHub user data
-fetch("https://api.github.com/users/js-goose")
+const userData = fetch("https://api.github.com/users/js-goose")
   .then((response) => response.json())
-  .then((response) => console.log(response));
+  .then((response) => {
+    userSection.innerHTML = `<img src=${response.avatar_url} height=100 width = 100>`
+    console.log(response);
+  })
+  .catch((error) =>
+    console.log("There was an error fetching the data: " + error)
+  );
+
+
