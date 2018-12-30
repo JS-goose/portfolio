7ed541120a0b244959c0b204ad80958448059f13
Jonathan Sexton <joncsexton@gmail.com>
2018-12-29 18:31:17 -0600

    changed structure of github section display

--- js/public.js
+++ js/public.js
@@ -1,10 +1,10 @@
-let userSection = document.querySelector(".user-data");
+let userImg = document.querySelector("#user-img");
 
 // fetch GitHub user data
 const userData = fetch("https://api.github.com/users/js-goose")
   .then((response) => response.json())
   .then((response) => {
-    userSection.innerHTML = `<img src=${response.avatar_url} height=100 width = 100>`
+    userImg.innerHTML = `<img src=${response.avatar_url} height=100 width = 100 alt='user image of js-goose'>`
     console.log(response);
   })
   .catch((error) =>
