c1d2e1b825a499072aaa5a0ef294ad82bcbd86dd
Jonathan Sexton <joncsexton@.com>
2019-01-15 17:08:32 -0600

    abstracted repos to its own call and converted to an IIFE

--- js/public.js
+++ js/public.js
@@ -1,25 +1,38 @@
 let userDataSection = document.querySelector(".user-data");
+// const repos = fetch("https://api.github.com/users/js-goose/repos");
+// const reposData = repos.json();
 
-const getUser = async () => {
+const getRepos = (async () => {
+  try {
+    const repos = await fetch("https://api.github.com/users/js-goose/repos");
+    const reposData = await repos.json();
+    console.log(reposData);
+  } catch (error) {
+    console.error(`There was an error fetching the data: ${error}`);
+  }
+})();
+
+// getRepos();
+
+const getUser = (async () => {
   try {
     const user = await fetch("https://api.github.com/users/js-goose");
-    const repos = await fetch("https://api.github.com/users/js-goose/repos")
-    const repoData = await repos.json();
+    // const repos = await fetch("https://api.github.com/users/js-goose/repos");
+    // const repoData = await repos.json();
     const userData = await user.json();
     console.log(userData);
-    console.log(repoData);
+    // console.log(repoData);
     const html = `
     <ul id="github-user-list">
       <li>GitHub Name: ${userData.name}</li> 
       <li><img src="${userData.avatar_url}"></li>
       <li>Public Repositories: ${userData.public_repos}</li>
-      <li><details>${repoData.name}</details></li>
     </ul>`;
 
     userDataSection.innerHTML = html;
   } catch (error) {
     console.error(`There was an error fetching the data: ${error}`);
   }
-};
+})();
 
-getUser();
+// getUser();
