da4063c33e1b309219027c98e5a8aa08988b78e5
Jonathan Sexton <joncsexton@.com>
2019-01-15 17:29:00 -0600

    brought repo call back inside get user and added to html template

--- js/public.js
+++ js/public.js
@@ -1,38 +1,30 @@
 let userDataSection = document.querySelector(".user-data");
-// const repos = fetch("https://api.github.com/users/js-goose/repos");
-// const reposData = repos.json();
 
-const getRepos = (async () => {
-  try {
-    const repos = await fetch("https://api.github.com/users/js-goose/repos");
-    const reposData = await repos.json();
-    console.log(reposData);
-  } catch (error) {
-    console.error(`There was an error fetching the data: ${error}`);
-  }
-})();
-
-// getRepos();
-
-const getUser = (async () => {
+const getUser = async (cb) => {
   try {
     const user = await fetch("https://api.github.com/users/js-goose");
-    // const repos = await fetch("https://api.github.com/users/js-goose/repos");
-    // const repoData = await repos.json();
     const userData = await user.json();
+    const repos = await fetch("https://api.github.com/users/js-goose/repos");
+    const reposData = await repos.json();
     console.log(userData);
-    // console.log(repoData);
+    console.log(reposData);
+    let repoNames = reposData.map((item)=> {
+      console.log(item.name)
+      return item.name
+    });
+    // console.log(reposData);
     const html = `
     <ul id="github-user-list">
       <li>GitHub Name: ${userData.name}</li> 
       <li><img src="${userData.avatar_url}"></li>
       <li>Public Repositories: ${userData.public_repos}</li>
+      <li><details>${repoNames}</details></li>
     </ul>`;
 
     userDataSection.innerHTML = html;
   } catch (error) {
     console.error(`There was an error fetching the data: ${error}`);
   }
-})();
+};
 
-// getUser();
+getUser();
