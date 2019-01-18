0e442a355493bb6fc46e976fa47d9f89ee9f8810
Jonathan Sexton <joncsexton@gmail.com>
2019-01-18 05:11:48 -0600

    abstracted the github user list UL to HTML instead of JS file

--- js/public.js
+++ js/public.js
@@ -1,6 +1,6 @@
-let userDataSection = document.querySelector(".user-data");
+const ghUserSection = document.querySelector("#github-user-list");
 
-const getUser = async (cb) => {
+const getUser = async ()=>{
   try {
     const user = await fetch("https://api.github.com/users/js-goose");
     const userData = await user.json();
@@ -10,32 +10,25 @@ const getUser = async (cb) => {
     console.log(reposData);
     let repoNames = reposData.map((item) => {
       const html = `
-      // TODO Refactor this ul into the HTML
-      <ul id="repo-list">
         <li>
           <a href="${item.clone_url}" target="_blank">${item.name}</a>
         </li>
-      </ul>`;
+      `;
       return html;
     });
-    // console.log(reposData);
     const html = `
-    // TODO Refactor this ul into the HTML
-    <ul id="github-user-list">
       <li>GitHub Name: ${userData.name}</li> 
       <li><img src="${userData.avatar_url}"></li>
       <li>Public Repositories: ${userData.public_repos}</li>
       <li>
         <details>
           <summary>All Public Repos</summary>
-          <ul>
             <li>${repoNames.join(" ")}</li>
-          </ul>
         </details>
       </li>
-    </ul>`;
+      `;
 
-    userDataSection.innerHTML = html;
+    ghUserSection.innerHTML = html;
   } catch (error) {
     console.error(`There was an error fetching the data: ${error}`);
   }
