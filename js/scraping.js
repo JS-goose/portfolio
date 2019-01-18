4fa21cb2e51179601e4987b163fdd192deb8d836
Jonathan Sexton <joncsexton@gmail.com>
2019-01-18 13:41:30 -0600

    trying to get images through scrape

--- js/scraping.js
+++ js/scraping.js
@@ -1,19 +1,43 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
-const heatMap = await page.evaluate(() => {
-  const svgImg = document.querySelector('#cal-heatmap');
-  const points = document.querySelector('.text-center points')
-})
+// const url = process.argv[2];
+// if (!url) {
+//   throw "Please provide a URL as the first argument!";
+// }
+
+// function run() {
+//   return new Promise(async (resolve, reject) => {
+//     try {
+//       const browser = await puppeteer.launch();
+//       const page = await browser.newPage();
+//       await page.goto("https://www.freecodecamp.org/js-goose");
+//       let data = await page.evaluate(() => {
+//         let results = [];
+//         const points = document.querySelector(".text-center points");
+//         return results.push(points);
+//       });
+//       browser.close();
+//       return resolve(data)
+//     } catch (error) {
+//       return reject(`Error in promise: ${error}`);
+//     }
+//   });
+// }
+// run().then(console.log).catch(console.error);
 
 // void (async () => {
-//     try {
-//         const browser = await puppeteer.launch();
-//         const page = await browser.newPage();
-//         await page.goto('https://www.freecodecamp.org/js-goose');
-//         await page.screenshot({path : '../img/page1.png'});
-//         await page.pdf({path: '../img/page1.pdf'});
-//         await browser.close();
-//     } catch(error) {
-//         console.error(`There was an error: ${error}`)
-//     }
+//   try {
+//     const browser = await puppeteer.launch();
+//     const page = await browser.newPage();
+//     await page.goto("https://www.freecodecamp.org/js-goose");
+
+//     const heatMap = await page.evaluate(() => {
+//       const svgImg = document.querySelector("#cal-heatmap");
+//       const points = document.querySelector(".text-center points");
+//       console.log(points);
+//     });
+//     await browser.close();
+//   } catch (error) {
+//     console.error(`There was an error: ${error}`);
+//   }
 // })();
