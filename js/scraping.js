ebd496098fa64263d85d9642020e19c5d6a81a5e
Jonathan Sexton <joncsexton@gmail.com>
2019-01-18 13:01:57 -0600

    setting up variables and fcc scraping function

--- js/scraping.js
+++ js/scraping.js
@@ -1,14 +1,19 @@
 const puppeteer = require('puppeteer');
 
-void (async () => {
-    try {
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-        await page.goto('https://www.freecodecamp.org/js-goose');
-        await page.screenshot({path : '../img/page1.png'});
-        await page.pdf({path: '../img/page1.pdf'});
-        await browser.close();
-    } catch(error) {
-        console.error(`There was an error: ${error}`)
-    }
-})();
+const heatMap = await page.evaluate(() => {
+  const svgImg = document.querySelector('#cal-heatmap');
+  const points = document.querySelector('.text-center points')
+})
+
+// void (async () => {
+//     try {
+//         const browser = await puppeteer.launch();
+//         const page = await browser.newPage();
+//         await page.goto('https://www.freecodecamp.org/js-goose');
+//         await page.screenshot({path : '../img/page1.png'});
+//         await page.pdf({path: '../img/page1.pdf'});
+//         await browser.close();
+//     } catch(error) {
+//         console.error(`There was an error: ${error}`)
+//     }
+// })();
