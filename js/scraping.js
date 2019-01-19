841be30ea10032ef0d9fa36ff87f8ac0ce328de9
Jonathan Sexton <joncsexton@gmail.com>
2019-01-19 15:24:57 -0600

    added attribution to scraping script

--- js/scraping.js
+++ js/scraping.js
@@ -1,3 +1,4 @@
+//Script courtesy of intoli @ https://intoli.com/blog/saving-images/
 const fs = require('fs');
 const puppeteer = require('puppeteer');
 
@@ -22,45 +23,3 @@ const puppeteer = require('puppeteer');
 
   await browser.close();
 })();
-
-// const url = process.argv[2];
-// if (!url) {
-//   throw "Please provide a URL as the first argument!";
-// }
-
-// function run() {
-//   return new Promise(async (resolve, reject) => {
-//     try {
-//       const browser = await puppeteer.launch();
-//       const page = await browser.newPage();
-//       await page.goto("https://www.freecodecamp.org/js-goose");
-//       let data = await page.evaluate(() => {
-//         let results = [];
-//         const points = document.querySelector(".text-center points");
-//         return results.push(points);
-//       });
-//       browser.close();
-//       return resolve(data)
-//     } catch (error) {
-//       return reject(`Error in promise: ${error}`);
-//     }
-//   });
-// }
-// run().then(console.log).catch(console.error);
-
-// void (async () => {
-//   try {
-//     const browser = await puppeteer.launch();
-//     const page = await browser.newPage();
-//     await page.goto("https://www.freecodecamp.org/js-goose");
-
-//     const heatMap = await page.evaluate(() => {
-//       const svgImg = document.querySelector("#cal-heatmap");
-//       const points = document.querySelector(".text-center points");
-//       console.log(points);
-//     });
-//     await browser.close();
-//   } catch (error) {
-//     console.error(`There was an error: ${error}`);
-//   }
-// })();
