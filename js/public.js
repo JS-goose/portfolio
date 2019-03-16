a21cb5e76fecdd275af8902ef6b22ab16263aa51
Jonathan Sexton <joncsexton@.com>
2019-03-16 14:48:41 -0500

    removed dark class from project overlays

--- js/public.js
+++ js/public.js
@@ -45,9 +45,14 @@ const modeBtn = document
   .querySelector("#light-dark-mode-container")
   .addEventListener("click", function() {
     const everything = document.querySelectorAll('*')
+    const projectTiles = document.querySelectorAll('.project-tile-overlay');
     everything.forEach((item) => {
       item.classList.toggle('dark');
     });
+
+    projectTiles.forEach((item)=> {
+      item.classList.remove('dark');
+    })
   });
 
 // Typeform button function
