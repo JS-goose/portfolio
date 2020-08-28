bc1347642fac15245a341b15f68e5901211f8f94
Jonathan Sexton <joncsexton@gmail.com>
2020-08-28 12:38:41 -0500

    fixed directory, updated packages, installed gatsby

--- version2/src/components/layout.js
+++ version2/src/components/layout.js
@@ -26,10 +26,20 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `0 1.0875rem 1.45rem`,
+        }}
+      >
         <main>{children}</main>
         <footer>
-          Jonathan Sexton © 2015 - {new Date().getFullYear()} 
+          © {new Date().getFullYear()}, Built with
+          {` `}
+          <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
+      </div>
     </>
   )
 }
