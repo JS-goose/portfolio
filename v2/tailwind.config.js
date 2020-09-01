602b66c6d3137e1b20a488be6d50da29e91e2ab4
Jonathan Sexton <joncsexton@gmail.com>
2020-09-01 12:08:25 -0500

    this is the actual change from previous commit

--- v2/tailwind.config.js
+++ v2/tailwind.config.js
@@ -1,11 +1,11 @@
 // See https://tailwindcss.com/docs/configuration for details
 module.exports = {
-  purge: ["./src/**/*.js"],
+  purge: ['./src/**/*.js'],
   theme: {},
   variants: {},
   // https://github.com/tailwindcss/custom-forms
-  plugins: [require("@tailwindcss/custom-forms")],
+  plugins: [require('@tailwindcss/custom-forms')],
   future: {
     removeDeprecatedGapUtilities: true,
-  }
+  },
 };
