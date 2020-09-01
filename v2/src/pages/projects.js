d90056ba3bf4f18da56cf66e092caa5f721ccbb2
Jonathan Sexton <joncsexton@gmail.com>
2020-09-01 12:18:16 -0500

    added top lvl pages for team/projects

---
+++ v2/src/pages/projects.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import SEO from '../components/seo';
+
+const Projects = () => {
+  return (
+    <>
+      <SEO
+        keywords={[`freelance`, `about`, `jonathan`, `sexton`, `development`, `website`]}
+        title="Projects"
+      />
+      <div>Hello from projects page!</div>
+    </>
+  );
+};
+
+export default Projects;
---
+++ v2/src/pages/team.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import SEO from '../components/seo';
+
+const Team = () => {
+    return (
+        <>
+          <SEO
+            keywords={[`freelance`, `team`, `jonathan`, `sexton`, `development`, `website`]}
+            title="Projects"
+          />
+          <div>Hello from Team page!</div>
+        </>
+      );
+}
+
+export default Team;
