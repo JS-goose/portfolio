42142e18b9ceeb9a755962323ec23343c22a7656
Jonathan Sexton <joncsexton@gmail.com>
2020-09-05 22:40:58 -0500

    Added my colors

--- v2/tailwind.config.js
+++ v2/tailwind.config.js
@@ -1,7 +1,15 @@
 // See https://tailwindcss.com/docs/configuration for details
 module.exports = {
   purge: ['./src/**/*.js'],
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        'myPurple': '421ED2',
+        'myRed': 'FF4945',
+        'myBlack': '161514'
+      }
+    }
+  },
   variants: {},
   // https://github.com/tailwindcss/custom-forms
   plugins: [require('@tailwindcss/custom-forms')],
