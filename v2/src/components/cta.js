90f19aeb5a133777eed8d19fa7438585bdbf4485
Jonathan Sexton <joncsexton@gmail.com>
2020-09-06 22:34:38 -0500

    added my image to cta

--- v2/src/components/cta.js
+++ v2/src/components/cta.js
@@ -1,19 +1,38 @@
 import React from 'react';
+import Image from 'gatsby-image';
+import { useStaticQuery, graphql } from 'gatsby';
 
 const CallToAction = () => {
+  const query = useStaticQuery(graphql`
+    query {
+      me: file(relativePath: { eq: "me also-min.png" }) {
+        childImageSharp {
+          fluid(quality: 100, maxWidth: 600) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+    }
+  `);
   return (
     <section className="bg-white h-full w-full">
-      <article>This is where the image will be</article>
+      <article>
+        <Image fluid={query.me.childImageSharp.fluid} />
+      </article>
       <article>
         <h4 className="text-2xl">Hi, I{`'`}m Jonathan</h4>
         <p>
-          I{`'`}m a freelance developer who specializes in helping people and businesses expand their
-          online presence, reach more customers, and broaden their audience! If you’re looking for a
-          developer to get your business website designed, built, and published for the world to see
-          then you’ve come to the right place my friend. Let’s launch your project together!
+          I{`'`}m a freelance developer who specializes in helping people and businesses expand
+          their online presence, reach more customers, and broaden their audience! If you’re looking
+          for a developer to get your business website designed, built, and published for the world
+          to see then you’ve come to the right place my friend. Let’s launch your project together!
         </p>
-        <a href="#" target="_blank" rel="noopener noreferrer">Hire Me For Your Project</a>
-        <a href="#" target="_blank" rel="noopener noreferrer">See My Services</a>
+        <a href="#" target="_blank" rel="noopener noreferrer">
+          Hire Me For Your Project
+        </a>
+        <a href="#" target="_blank" rel="noopener noreferrer">
+          See My Services
+        </a>
       </article>
     </section>
   );
