e85319556676ef697a5c1a82e1097e5ca55d9f2f
Jonathan Sexton <joncsexton@gmail.com>
2020-09-12 18:38:48 -0500

    created services component

---
+++ v2/src/components/servicesList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const ServicesList = () => {
+    return (
+        <div>
+            <ul>
+                <li>Service One</li>
+                <li>Service Two</li>
+                <li>Service Three</li>
+                <li>Service Four</li>
+                <li>Service Five</li>
+                <li>Service Six</li>
+            </ul>
+        </div>
+    )
+}
+
+export default ServicesList
