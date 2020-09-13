e0e41c4b2cd08f6bc1282228f4efd6fcd2a438de
Jonathan Sexton <joncsexton@gmail.com>
2020-09-12 21:05:25 -0500

    added layoout and services loop

--- v2/src/components/servicesList.js
+++ v2/src/components/servicesList.js
@@ -1,22 +1,39 @@
 import React from 'react';
 
+const services = [{ number: `1`, desc: `text about the service here` }, 
+{ number: `2`, desc: `text about the service here` }, 
+{ number: `3`, desc: `text about the service here` }, 
+{ number: `4`, desc: `text about the service here` }, 
+{ number: `5`, desc: `text about the service here` }, 
+{ number: `6`, desc: `text about the service here` }, 
+{ number: `7`, desc: `text about the service here` },];
+
 const ServicesList = () => {
     return (
-        <div className="bg-white border border-white grid-row-3">
-            <ul>
-                <li>Service One</li>
-                <li>Service Two</li>
-                <li>Service Three</li>
-                <li>Service Four</li>
-                <li>Service Five</li>
-                <li>Service Six</li>
-                <li>Service One</li>
-                <li>Service Two</li>
-                <li>Service Three</li>
-                <li>Service Four</li>
-                <li>Service Five</li>
-                <li>Service Six</li>
-            </ul>
+        <div className="bg-white flex flex-col justify-between h-full mt-12 p-1 border border-white grid-row-3">
+            <div className="services-list border border-red-500 p-2">
+                <ul>
+                    {services.map((service) => {
+                        return <li key={service.number}>{service.number}. {service.desc}</li>
+                    })}
+                </ul>
+            </div>
+            <div className="why-hire-me border border-purple-500 p-2">
+                <ul>
+                    <li>Hire me because [reason]</li>
+                    <li>Hire me because [reason]</li>
+                    <li>Hire me because [reason]</li>
+                    <li>Hire me because [reason]</li>
+                    <li>Hire me because [reason]</li>
+                    <li>Hire me because [reason]</li>
+                    <li>Hire me because [reason]</li>
+                    <li>Hire me because [reason]</li>
+                    <li>Hire me because [reason]</li>
+                    <li>Hire me because [reason]</li>
+                    <li>Hire me because [reason]</li>
+                    <li>Hire me because [reason]</li>
+                </ul>
+            </div>
         </div>
     )
 }
