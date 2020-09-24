1ad9bf0277a5ec5f9c9d093cae3dd6378be446dc
Jonathan Sexton <joncsexton@gmail.com>
2020-09-24 13:25:32 -0500

    changed up customers

--- v2/src/components/getStarted.js
+++ v2/src/components/getStarted.js
@@ -4,8 +4,8 @@ const GetStarted = () => {
   const testimonials = [
     { name: `Tracy`, company: `Tracy's Upholstery`, comment: `Testimonial here` },
     { name: `Stu`, company: `Borealis Web Development`, comment: `Testimonial here` },
-    {name: `Caleb Helton`, company: `Long State Breaks`, comment: `Testimonial here`},
-    {name: `Keith Olson`, company: `Conduit PC Shop`, comment: `Testimonial here`},
+    { name: `Caleb Helton`, company: `Long State Breaks`, comment: `Testimonial here` },
+    { name: `Keith Olson`, company: `Conduit PC Shop`, comment: `Testimonial here` },
     // {name: `Steve Flannery`, company: `Steve's PC Shop`, comment: `Testimonial here`},
   ];
   return (
@@ -17,7 +17,9 @@ const GetStarted = () => {
       <article>
         <ul>
           {testimonials.map((test) => (
-            <li key={test.name}>{test.name} - {test.comment}</li>
+            <li key={test.name}>
+              {test.name} - {test.comment}
+            </li>
           ))}
         </ul>
       </article>
