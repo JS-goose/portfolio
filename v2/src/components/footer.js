f0aec01d38d138ab2fd0ee541ba5e0b315dc1be4
Jonathan Sexton <joncsexton@gmail.com>
2020-09-24 07:49:02 -0500

    added basics to footer

--- v2/src/components/footer.js
+++ v2/src/components/footer.js
@@ -1,8 +1,12 @@
+import { Link } from "gatsby";
 import React from "react";
+
+const d = new Date();
+const year = d.getFullYear();
 
 const Footer = () => (
   <footer className="text-white">
-    <article className="flex justify-between p-4 mx-auto text-sm md:p-8">
+    <article className="flex justify-between p-4 mx-auto text-sm md:p-8 bg-myRed">
       <div>
         <ul>
           <li>
@@ -22,22 +26,22 @@ const Footer = () => (
       </div>
       <div>
         <ul>
-          <li>Freelance Developer (links back to home)</li>
-          <li></li>
-          <li></li>
-          <li></li>
+          <li>
+            <Link to="/">Freelance Developer</Link>
+          </li>
+          <li>Freelance Consultant (links to Consultant page)</li>
+          <li>Resume (download)</li>
         </ul>
       </div>
     </article>
     <article className="flex justify-between p-4 mx-auto text-sm md:p-8">
       <p>
         Created by{` `}
-        <a className="font-bold no-underline" href="https://bryant.io" target="_blank" rel="noopener noreferrer">
-          Jonathan Sexton
-        </a>
+        Jonathan Sexton &copy; {year}
       </p>
 
       <p>
+        I code on{` `}
         <a
           className="font-bold no-underline"
           href="https://github.com/JS-goose"
