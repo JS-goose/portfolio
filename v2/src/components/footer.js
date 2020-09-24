61671dd397d4e7c97012ebce691ad9ff9f4f16bd
Jonathan Sexton <joncsexton@gmail.com>
2020-09-24 00:06:38 -0500

    working on the footer

--- v2/src/components/footer.js
+++ v2/src/components/footer.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => (
-  <footer>
-    <nav className="flex justify-between max-w-4xl p-4 mx-auto text-sm md:p-8">
+  <footer className="text-white">
+    <article className="flex justify-between p-4 mx-auto text-sm md:p-8">
+      <div>
+        <ul>
+          <li>
+            <a href="mailto:hello@jonathansexton.me?subject=Email_inquiry_from_jonathansexton.me">Email Me</a>
+          </li>
+          <li>About Me</li>
+          <li>Blog</li>
+        </ul>
+      </div>
+      <div>
+        <ul>
+          <li>Follow me on Twitter</li>
+          <li>Follow me on Instagram</li>
+          <li>Follow me on LinkedIn</li>
+          <li>Follow me on Facebook</li>
+        </ul>
+      </div>
+      <div>
+        <ul>
+          <li>Freelance Developer (links back to home)</li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </article>
+    <article className="flex justify-between p-4 mx-auto text-sm md:p-8">
       <p>
         Created by{` `}
-        <a
-          className="font-bold no-underline"
-          href="https://bryant.io"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="font-bold no-underline" href="https://bryant.io" target="_blank" rel="noopener noreferrer">
           Jonathan Sexton
         </a>
       </p>
@@ -20,12 +42,11 @@ const Footer = () => (
           className="font-bold no-underline"
           href="https://github.com/JS-goose"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           GitHub
         </a>
       </p>
-    </nav>
+    </article>
   </footer>
 );
 
