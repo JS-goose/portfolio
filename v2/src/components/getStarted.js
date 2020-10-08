72b50326831f37796cc1e7358de8cbd8023f8db8
Jonathan Sexton <joncsexton@gmail.com>
2020-10-07 20:58:29 -0500

    added submit button and styling

--- v2/src/components/getStarted.js
+++ v2/src/components/getStarted.js
@@ -10,7 +10,7 @@ const GetStarted = () => {
   ];
   return (
     <section className="p-2 flex flex-col items-center">
-      <article className="border border-1 p-12 rounded shadow mb-8">
+      <article className="border border-1 p-12 rounded-sm shadow mb-8">
         <h2>Let&apos;s get started on your project!</h2>
         <form name="contact" method="POST" data-netlify="true" netlify-honeypot="bots-say-what">
           <p className="invisible">
@@ -30,7 +30,7 @@ const GetStarted = () => {
                 required
                 minLength="5"
                 maxLength="100"
-                className="border border-1 p-2"
+                className="border border-1 p-2 rounded-sm"
                 placeholder="First and Last Names"
                 aria-placeholder="First and Last Names"
               />
@@ -45,7 +45,7 @@ const GetStarted = () => {
                 required
                 minLength="5"
                 maxLength="100"
-                className="border border-1 p-2"
+                className="border border-1 p-2 rounded-sm"
                 placeholder="name@domain.com"
                 aria-placeholder="name@domain.com"
               />
@@ -58,7 +58,7 @@ const GetStarted = () => {
                 id="phone-number"
                 aria-required
                 required
-                className="border border-1 p-2"
+                className="border border-1 p-2 rounded-sm"
                 placeholder="888-555-1234"
               />
             </p>
@@ -72,9 +72,26 @@ const GetStarted = () => {
                 aria-required
                 required
                 maxLength="300"
-                className="border border-1 p-2"></textarea>
+                className="border border-1 p-2 rounded-sm"></textarea>
             </p>
           </fieldset>
+          <button
+            type="submit"
+            className="flex border border-1 shadow-md text-lg p-4 rounded-sm transition duration-500 hover:underline hover:text-white hover:bg-gradient-to-r from-myPurple to-myRed">
+            <span className="pr-2">Submit Request</span>{" "}
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <g id="done_outline_24px">
+                <path
+                  id="icon/action/done_outline_24px"
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M8.43005 13.44L19.77 2.1L24 6.33L8.43005 21.9L0 13.47L4.22998 9.24L8.43005 13.44ZM21.1699 6.33L19.7699 4.93L8.42993 16.27L4.22998 12.07L2.82996 13.47L8.42993 19.07L21.1699 6.33Z"
+                  fill="white"
+                  fillOpacity="0.54"
+                />
+              </g>
+            </svg>
+          </button>
         </form>
       </article>
       <article>
