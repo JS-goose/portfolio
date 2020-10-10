9a821856910e6225e388681246d8a02b112bf9d3
Jonathan Sexton <joncsexton@gmail.com>
2020-10-10 17:27:41 -0500

    removed commented out code

--- v2/src/components/getStarted.js
+++ v2/src/components/getStarted.js
@@ -1,13 +1,6 @@
 import React from "react";
 
 const GetStarted = () => {
-  // const testimonials = [
-  //   { name: `Tracy Johnson`, company: `Tracy's Upholstery`, comment: `Testimonial here` },
-  //   { name: `Stu Finn`, company: `Borealis Web Development`, comment: `Testimonial here` },
-  //   { name: `Caleb Helton`, company: `Long State Breaks`, comment: `Testimonial here` },
-  //   { name: `Keith Olson`, company: `Conduit PC Shop`, comment: `Testimonial here` },
-  //   // {name: `Steve Flannery`, company: `Steve's PC Shop`, comment: `Testimonial here`},
-  // ];
   return (
     <section className="p-2 flex flex-col items-center">
       <article className="border border-1 p-12 rounded-sm shadow mb-8">
@@ -94,15 +87,6 @@ const GetStarted = () => {
           </button>
         </form>
       </article>
-      {/* <article>
-        <ul>
-          {testimonials.map((test) => (
-            <li key={test.name}>
-              {test.name} - {test.comment}
-            </li>
-          ))}
-        </ul>
-      </article> */}
     </section>
   );
 };
