363bb989c68388e184187b059e1c7edc6f288d22
Jonathan Sexton <joncsexton@gmail.com>
2020-10-14 22:37:35 -0500

    responsive changes

--- v2/src/components/testimonials.js
+++ v2/src/components/testimonials.js
@@ -7,25 +7,35 @@ const Testimonials = () => {
       name: `Stu Finn`,
       company: `Borealis Web Development`,
       title: `Owner/Developer`,
-      comment: `Testimonial here`,
+      comment: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
       website: `https://borealisweb.ca`,
     },
     {
       name: `Tracy Johnson`,
       company: `Tracy's Upholstery`,
       title: `Owner`,
-      comment: `Testimonial here`,
+      comment: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
       website: `https://https://www.tracys-upholstery.com/`,
     },
     {
       name: `Michael Masor`,
       company: `Annuity Market Pro`,
       title: `CEO/Founder`,
-      comment: `Testimonial here`,
+      comment: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
       website: `https://www.annuitymarketpro.com/`,
     },
-    { name: `Caleb Helton`, company: `Long State Breaks`, comment: `We're a niche market company`, website: `` },
-    { name: `Keith Olson`, company: `Conduit PC Shop`, comment: `Testimonial here`, website: `` },
+    {
+      name: `Caleb Helton`,
+      company: `Long State Breaks`,
+      comment: `We're a niche market company, so it was nice to find someone like Jonathan who could narrow down pain points and address them quickly`,
+      website: ``,
+    },
+    {
+      name: `Keith Olson`,
+      company: `Conduit PC Shop`,
+      comment: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+      website: ``,
+    },
     // {name: `Steve Flannery`, company: `Steve's PC Shop`, comment: `Testimonial here`, website: `https://www.localcomputer.expert/`},
   ];
 
@@ -48,12 +58,12 @@ const Testimonials = () => {
         <ul>
           {testimonials.map((card, index) => (
             <li key={index} data-key={index + 1}>
-              <div className="flex shadow rounded-sm p-6">
+              <div className="flex shadow rounded-sm p-6 items-center justify-around">
                 {/* Avatars courtesy of Undraw.co */}
                 {index + 1 === 2 ? (
-                  <img src={query.icon2.publicURL} className="sm:h-26 md:h-24 lg:h-32 mr-4" alt="a female avatar " />
+                  <img src={query.icon2.publicURL} className="sm:h-20 md:h-26 lg:h-32 mr-4" alt="a female avatar " />
                 ) : (
-                  <img src={query.icon.publicURL} className="sm:h-26 md:h-24 lg:h-32 mr-4" alt="a male avatar" />
+                  <img src={query.icon.publicURL} className="sm:h-20 md:h-26 lg:h-32 mr-4" alt="a male avatar" />
                 )}
                 <div>
                   <h4 className="text-2xl">{card.name}</h4>
@@ -61,7 +71,7 @@ const Testimonials = () => {
                   <p>
                     <a href={card.website}>{card.company}</a>
                   </p>
-                  <p>{card.comment}</p>
+                  <p className="sm:max-w-xs md:max-w-sm lg:max-w-lg">{card.comment}</p>
                 </div>
               </div>{" "}
             </li>
