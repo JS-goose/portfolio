e31a2569b033e9b106c29e223e22cca5dc78f363
Jonathan Sexton <joncsexton@gmail.com>
2020-10-14 22:28:57 -0500

    added avatar images

--- v2/src/components/testimonials.js
+++ v2/src/components/testimonials.js
@@ -1,23 +1,26 @@
 import React from "react";
-import Image from "gatsby-image";
+import { graphql, useStaticQuery } from "gatsby";
 
 const Testimonials = () => {
   const testimonials = [
     {
       name: `Stu Finn`,
       company: `Borealis Web Development`,
+      title: `Owner/Developer`,
       comment: `Testimonial here`,
       website: `https://borealisweb.ca`,
     },
     {
       name: `Tracy Johnson`,
       company: `Tracy's Upholstery`,
+      title: `Owner`,
       comment: `Testimonial here`,
       website: `https://https://www.tracys-upholstery.com/`,
     },
     {
       name: `Michael Masor`,
       company: `Annuity Market Pro`,
+      title: `CEO/Founder`,
       comment: `Testimonial here`,
       website: `https://www.annuitymarketpro.com/`,
     },
@@ -26,18 +29,40 @@ const Testimonials = () => {
     // {name: `Steve Flannery`, company: `Steve's PC Shop`, comment: `Testimonial here`, website: `https://www.localcomputer.expert/`},
   ];
 
+  const query = useStaticQuery(graphql`
+    query {
+      icon: file(relativePath: { eq: "undraw_profile_pic_ic5t.svg" }) {
+        extension
+        publicURL
+      }
+      icon2: file(relativePath: { eq: "undraw_female_avatar_w3jk.svg" }) {
+        extension
+        publicURL
+      }
+    }
+  `);
+
   return (
     <section className="bg-white w-full flex items-center justify-center">
       <article className="items-center">
         <ul>
           {testimonials.map((card, index) => (
             <li key={index} data-key={index + 1}>
-              <div className="border border-myPurple ">
-                <h4 className="text-2xl">{card.name}</h4>
-                <p>{card.comment}</p>
-                <p>
-                  <a href={card.website}>{card.company}</a>
-                </p>
+              <div className="flex shadow rounded-sm p-6">
+                {/* Avatars courtesy of Undraw.co */}
+                {index + 1 === 2 ? (
+                  <img src={query.icon2.publicURL} className="sm:h-26 md:h-24 lg:h-32 mr-4" alt="a female avatar " />
+                ) : (
+                  <img src={query.icon.publicURL} className="sm:h-26 md:h-24 lg:h-32 mr-4" alt="a male avatar" />
+                )}
+                <div>
+                  <h4 className="text-2xl">{card.name}</h4>
+                  <p>{card.title}</p>
+                  <p>
+                    <a href={card.website}>{card.company}</a>
+                  </p>
+                  <p>{card.comment}</p>
+                </div>
               </div>{" "}
             </li>
           ))}
