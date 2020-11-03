eb364bc6dcdf2fd2c00f7e87e667bb058b02fdb3
Jonathan Sexton <joncsexton@gmail.com>
2020-11-02 19:20:18 -0600

    fixed issue with header link

--- v2/src/components/header.js
+++ v2/src/components/header.js
@@ -1,6 +1,6 @@
-import { graphql, useStaticQuery, Link } from "gatsby";
-import React, { useState } from "react";
-import Image from "gatsby-image";
+import { graphql, useStaticQuery, Link } from 'gatsby';
+import React, { useState } from 'react';
+import Image from 'gatsby-image';
 
 function Header() {
   const [isExpanded, toggleExpansion] = useState(false);
@@ -26,22 +26,22 @@ function Header() {
   `);
 
   return (
-    <header className="bg-myPurple text-white z-10">
-      <div className="flex flex-wrap items-center md:justify-center lg:justify-evenly max-w-6xl lg:max-w-full">
-        <span className="flex items-center">
-          <Link to="/" onMouseLeave={toggleHover} onMouseEnter={toggleHover2}>
-            <Image fluid={queryData.brand.childImageSharp.fluid} className={hovered ? "logoNotHovered" : "logo"} />
+    <header className='bg-myPurple text-white z-10'>
+      <div className='flex flex-wrap items-center md:justify-center lg:justify-evenly max-w-6xl lg:max-w-full'>
+        <span className='flex items-center'>
+          <Link to='/' onMouseLeave={toggleHover} onMouseEnter={toggleHover2}>
+            <Image fluid={queryData.brand.childImageSharp.fluid} className={hovered ? 'logoNotHovered' : 'logo'} />
           </Link>
-          <p className="mb-6">Jonathan Sexton</p>
+          <p className='mb-6'>Jonathan Sexton</p>
         </span>
 
         <button
-          type="button"
-          className="items-center block px-3 py-2 border-white rounded-sm md:hidden"
+          type='button'
+          className='items-center block px-3 py-2 border-white rounded-sm md:hidden'
           onClick={() => toggleExpansion(!isExpanded)}>
-          <svg className="w-3 h-3 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+          <svg className='w-3 h-3 fill-current' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'>
             <title>Menu</title>
-            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+            <path d='M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z' />
           </svg>
         </button>
 
@@ -77,11 +77,17 @@ function Header() {
               title: `Hire Me`,
               classname: `pt-2 pr-2 pl-2 pb-1 rounded-sm text-2xl hover:text-myRed ml-4 bg-myRed hover:bg-white hover:underline`,
             },
-          ].map((link) => (
-            <Link className={link.classname} key={link.title} to={link.route} rel="noopener noreferrer">
-              {link.title}
-            </Link>
-          ))}
+          ].map((link) =>
+            link.route === 'https://jonathansexton.me/blog' ? (
+              <a href='https://jonathansexton.me/blog' className={link.classname} key={link.title}>
+                Articles
+              </a>
+            ) : (
+              <Link className={link.classname} key={link.title} to={link.route} rel='noopener noreferrer'>
+                {link.title}
+              </Link>
+            )
+          )}
         </nav>
       </div>
     </header>
