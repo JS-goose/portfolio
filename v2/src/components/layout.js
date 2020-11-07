873f3346fd8721ac07ab1a5cc8fb8027b3bacdd9
Jonathan Sexton <joncsexton@gmail.com>
2020-11-06 21:12:41 -0600

    moved footer outside of layout wrapper

--- v2/src/components/layout.js
+++ v2/src/components/layout.js
@@ -6,15 +6,13 @@ import Footer from './footer';
 
 function Layout({ children }) {
   return (
-    // layout-wrapper flex flex-col min-h-screen font-sans text-myBlack
-    <div className="layout-wrapper sm:max-w-5xl md:max-w-4xl sm:mx-auto lg:max-w-full">
-      <Header />
-      {/* grid grid-flow-col grid-cols-1 grid-rows-3 gap-y-12 h-screen justify-between flex-1 w-full */}
-      <main>
-        {children}
-      </main>
+    <>
+      <div className='layout-wrapper sm:max-w-5xl md:max-w-4xl sm:mx-auto lg:max-w-full'>
+        <Header />
+        <main>{children}</main>
+      </div>
       <Footer />
-    </div>
+    </>
   );
 }
 
