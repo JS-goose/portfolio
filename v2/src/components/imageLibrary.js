4b1ada9f2219a325f38b5c09c55fb2b3efbd0fa9
Jonathan Sexton <joncsexton@gmail.com>
2020-11-24 20:50:31 -0600

    export image library

--- v2/src/components/imageLibrary.js
+++ v2/src/components/imageLibrary.js
@@ -1,10 +1,12 @@
 const imageLibrary = {
-    icons: {
-        brand: `../images/icons/Brand-min.png`,
-        check: `../images/icons/check.svg`,
-        check_purple: `../images/icons/check_purple.svg`,
-        scroll: `../images/icons/scroll.svg`,
-        scroll_white: `../images/icons/scroll_white.svg`,
-        hand: `../images/icons/waving hand.png`    
-    }
-}
+  icons: {
+    brand: `../images/icons/Brand-min.png`,
+    check: `../images/icons/check.svg`,
+    check_purple: `../images/icons/check_purple.svg`,
+    scroll: `../images/icons/scroll.svg`,
+    scroll_white: `../images/icons/scroll_white.svg`,
+    hand: `../images/icons/waving hand.png`,
+  },
+};
+
+export default imageLibrary;
