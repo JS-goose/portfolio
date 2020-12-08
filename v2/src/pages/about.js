a8e7b869cf8839b0a99e02b41c0c839c0813603e
Jonathan Sexton <joncsexton@gmail.com>
2020-12-08 06:02:53 -0600

    added copy containers

--- v2/src/pages/about.js
+++ v2/src/pages/about.js
@@ -16,14 +16,13 @@ function AboutPage() {
           }
         }
       }
-
     }
   `);
   return (
     <section className='flex flex-col min-w-full max-w-screen items-center'>
       <SEO keywords={[`gatsby`, `tailwind`, `react`, `tailwindcss`]} title='About' />
       <article className='about-containers bg-white p-4 flex sm:leading-8 sm:text-base md:text-lg lg:text-xl'>
-        <div>
+        <div className='about-copy-containers'>
           <H1>Who I Am</H1>
           <p className='text-2xl mb-4'>
             Hello! I&apos;m Jonathan and I&apos;ve been creating in one form or another since I began exploring the
@@ -42,47 +41,53 @@ function AboutPage() {
         <Image fluid={query.golfing.childImageSharp.fluid} className='aboutImg w-full h-auto' />
       </article>
       <article className='about-containers bg-white p-4 flex flex-col sm:leading-8 sm:text-base md:text-lg lg:text-xl'>
-        <H1>What I Love</H1>
-        <p>
-          When I&apos;m not working I enjoy spending time with my <del>girlfriend</del> &nbsp;
-          <del>fiance</del> wife, our <del>daughter</del> daughters, and playing a few video games when I get the chance
-          (rarely ever :D). I love wine, cooking, sports, technology, and the outdoors. If I&apos;m not building a
-          project or fixing something around the house you&apos;ll likely find me outside on an adventure with the two
-          most important people in my life.
-        </p>
+        <div className='about-copy-containers'>
+          <H1>What I Love</H1>
+          <p>
+            When I&apos;m not working I enjoy spending time with my <del>girlfriend</del> &nbsp;
+            <del>fiance</del> wife, our <del>daughter</del> daughters, and playing a few video games when I get the
+            chance (rarely ever :D). I love wine, cooking, sports, technology, and the outdoors. If I&apos;m not
+            building a project or fixing something around the house you&apos;ll likely find me outside on an adventure
+            with the two most important people in my life.
+          </p>
+        </div>
       </article>
       <article className='about-containers bg-white p-4 flex flex-col sm:leading-8 sm:text-base md:text-lg lg:text-xl'>
-        <H1>What I Do</H1>
-        <p>
-          I&apos;m a freelance web developer who primarily works with small businesses to generate more leads, reach
-          their customers, and increase revenue.
-        </p>
-        <p>
-          I&apos;m passionate about designing, developing, and deploying easy to use and user friendly websites.
-          I&apos;ve always been passionate about finding creative solutions to problems and I apply that to everything I
-          do. My specialty is full website development from the ground up using semantic HTML, CSS, and JavaScript.
-        </p>
-        <p>
-          If you have any questions about me or would like to talk, all my social media links can be found at the top
-          and bottom of this page. I would love to connect with you! You can also swing by my blog and read some
-          articles to see what I&apos;m up to.
-        </p>
-        <p>
-          I love using technology to bring people together and sharing everything I&apos;ve learned. I even have a
-          &nbsp;
-          <a href='https://jonathansexton.me/blog' target='_blank' rel='noopener noreferrer'>
-            blog
-          </a>
-          &nbsp; where I write articles and share resources.
-        </p>
+        <div className='about-copy-containers'>
+          <H1>What I Do</H1>
+          <p>
+            I&apos;m a freelance web developer who primarily works with small businesses to generate more leads, reach
+            their customers, and increase revenue.
+          </p>
+          <p>
+            I&apos;m passionate about designing, developing, and deploying easy to use and user friendly websites.
+            I&apos;ve always been passionate about finding creative solutions to problems and I apply that to everything
+            I do. My specialty is full website development from the ground up using semantic HTML, CSS, and JavaScript.
+          </p>
+          <p>
+            If you have any questions about me or would like to talk, all my social media links can be found at the top
+            and bottom of this page. I would love to connect with you! You can also swing by my blog and read some
+            articles to see what I&apos;m up to.
+          </p>
+          <p>
+            I love using technology to bring people together and sharing everything I&apos;ve learned. I even have a
+            &nbsp;
+            <a href='https://jonathansexton.me/blog' target='_blank' rel='noopener noreferrer'>
+              blog
+            </a>
+            &nbsp; where I write articles and share resources.
+          </p>
+        </div>
       </article>
       <article className='about-containers bg-white p-4 flex flex-col sm:leading-8 sm:text-base md:text-lg lg:text-xl'>
-        <H1>Community Involvement</H1>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry
-          standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
-          make a type specimen book.
-        </p>
+        <div className='about-copy-containers'>
+          <H1>Community Involvement</H1>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry
+            standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
+            make a type specimen book.
+          </p>
+        </div>
       </article>
       <article
         className='about-containers md:p-2 flex flex-col items-center sm:text-base md:text-lg lg:text-xl'
