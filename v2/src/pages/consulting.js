7d1d88c3f5c0f1d005314f823b621e7423a3d014
Jonathan Sexton <joncsexton@gmail.com>
2021-02-10 21:02:28 -0600

    finally got content to center on page :)

--- v2/src/pages/consulting.js
+++ v2/src/pages/consulting.js
@@ -1,9 +1,22 @@
+import { useStaticQuery, graphql } from 'gatsby';
 import React from 'react';
 import SEO from '../components/seo';
+import Image from 'gatsby-image';
 
 const Consulting = () => {
+  const query = useStaticQuery(graphql`
+    query {
+      me: file(relativePath: { eq: "me-min.png" }) {
+        childImageSharp {
+          fluid(quality: 100, maxWidth: 600) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+    }
+  `);
   return (
-    <section className='flex flex-col min-w-full max-w-screen items-center'>
+    <section className='flex flex-col w-screen items-center'>
       <SEO
         keywords={[
           `boerne web developer`,
@@ -16,21 +29,26 @@ const Consulting = () => {
         title='Consulting'
       />
       <article className='consulting-inner-container bg-white p-4 sm:leading-8 sm:text-base md:text-lg lg:text-xl'>
-        <h1 className='sm:text-lg md:text-xl lg:text-2xl xl:text-3xl'>
-          Looking for a reliable partner to help with your existing website or app? 
-        </h1>
-        <p>Here are some ways I can help you with your current solution:</p>
-        <div>
-          <ul>
-            <li>Item #1</li>
-            <li>Item #2</li>
-            <li>Item #3</li>
-            <li>Item #4</li>
-            <li>Item #5</li>
-            <li>Item #6</li>
-            <li>Item #7</li>
-            <li>Item #8</li>
-          </ul>
+        <div className='flex'>
+          <div className='consulting-img-container'>
+            <Image fluid={query.me.childImageSharp.fluid} className='me-img' />
+          </div>
+          <div className='consulting-content-container p-2'>
+            <h1 className='sm:text-lg md:text-xl lg:text-2xl xl:text-3xl'>
+              Looking for a reliable partner to help with your existing website or application? 
+            </h1>
+            <p>Here are some ways I can help you with your current solution: </p>
+            <ul>
+              <li>Item #1</li>
+              <li>Item #2</li>
+              <li>Item #3</li>
+              <li>Item #4</li>
+              <li>Item #5</li>
+              <li>Item #6</li>
+              <li>Item #7</li>
+              <li>Item #8</li>
+            </ul>
+          </div>
         </div>
       </article>
     </section>
