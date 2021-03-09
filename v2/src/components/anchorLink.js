d3fcf8245feafa8771bf9b4de03bcaac1e823d08
Jonathan Sexton <joncsexton@gmail.com>
2021-03-08 22:15:57 -0600

    notes

--- v2/src/components/anchorLink.js
+++ v2/src/components/anchorLink.js
@@ -3,6 +3,7 @@ import { Link } from 'gatsby'
 
 const AnchorLink = (props) => {
     // ! This needs work before it's ready to use - standard class as well as props.class needs to be added
+    // ! Maybe I can look at styles in this component instead of a css file because of the content/width needing to be in the same rule
     if (props.linkType === 'internal') {
         return <Link to={props.linkAddress}>{props.addressText}</Link>
     } else {
