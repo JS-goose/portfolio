1a8d2e9ea442a110c350c0964ebb80f5cf88bda7
Jonathan Sexton <joncsexton@gmail.com>
2021-03-12 13:44:40 -0600

    added pseudo class variants

--- v2/tailwind.config.js
+++ v2/tailwind.config.js
@@ -1,4 +1,5 @@
 // See https://tailwindcss.com/docs/configuration for details
+const plugin = require('tailwindcss/plugin')
 module.exports = {
   purge: ["./src/**/*.js"],
   important: true,
@@ -12,13 +13,31 @@ module.exports = {
     },
   },
   // https://github.com/tailwindcss/custom-forms
-  plugins: [require("@tailwindcss/custom-forms")],
+  plugins: [require("@tailwindcss/custom-forms"), require("tailwindcss-pseudo-elements"), plugin(function ({ addUtilities }) {
+    addUtilities(
+      {
+        '.empty-content': {
+          content: "''",
+        },
+      },
+      ['before']
+    )
+  })],
   variants: {
+    extend: {
+    },
     backgroundImage: [`responsive`, `hover`, `focus`],
-    backgroundColor: [`responsive`, `hover`, `focus`, `active`],
+    backgroundColor: [`responsive`, `hover`, `focus`, `active`, `before`],
     textColor: [`responsive`, `hover`, `focus`, `active`],
     borderColor: [`responsive`, `hover`, `focus`],
     borderWidth: [`responsive`],
+    width: [`before`, `hover`],
+    height: [`before`, `hover`],
+    position: [`before`, `hover`],
+    margin: [`before`, `hover`],
+    transform: [`before`, `hover`],
+    transition: [`before`, `hover`],
+    scale: [`before`, `hover`]
   },
   corePlugins: {
     fontFamily: false,
