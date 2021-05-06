371abeb8b4372b7ff6cc59d0ad7b5beeaee66090
Jonathan Sexton <joncsexton@.com>
2021-05-06 13:23:15 -0500

    changes to the testimonials section

--- v2/src/components/testimonials.js
+++ v2/src/components/testimonials.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { graphql, useStaticQuery } from 'gatsby';
+import React from "react";
+import { graphql, useStaticQuery } from "gatsby";
 import { GatsbyImage } from "gatsby-plugin-image";
 
 const Testimonials = () => {
@@ -40,109 +40,93 @@ const Testimonials = () => {
     // {name: `Steve Flannery`, company: `Steve's PC Shop`, comment: `Testimonial here`, website: `https://www.localcomputer.expert/`},
   ];
 
-  const query = useStaticQuery(graphql`{
-  stu: file(relativePath: {eq: "Stu.jpg"}) {
-    childImageSharp {
-      gatsbyImageData(
-        quality: 100
-        width: 250
-        placeholder: TRACED_SVG
-        layout: CONSTRAINED
-      )
+  const query = useStaticQuery(graphql`
+    {
+      stu: file(relativePath: { eq: "Stu.jpg" }) {
+        childImageSharp {
+          gatsbyImageData(quality: 100, width: 250, placeholder: TRACED_SVG, layout: CONSTRAINED)
+        }
+      }
+      tracy: file(relativePath: { eq: "tracy-min.jpg" }) {
+        childImageSharp {
+          gatsbyImageData(quality: 100, width: 250, placeholder: TRACED_SVG, layout: CONSTRAINED)
+        }
+      }
+      mike: file(relativePath: { eq: "mike-min.jpg" }) {
+        childImageSharp {
+          gatsbyImageData(quality: 100, width: 250, placeholder: TRACED_SVG, layout: CONSTRAINED)
+        }
+      }
+      caleb: file(relativePath: { eq: "caleb-min.jpg" }) {
+        childImageSharp {
+          gatsbyImageData(quality: 100, width: 250, placeholder: TRACED_SVG, layout: CONSTRAINED)
+        }
+      }
+      keith: file(relativePath: { eq: "keith.jpg" }) {
+        childImageSharp {
+          gatsbyImageData(quality: 100, width: 250, placeholder: TRACED_SVG, layout: CONSTRAINED)
+        }
+      }
     }
-  }
-  tracy: file(relativePath: {eq: "tracy-min.jpg"}) {
-    childImageSharp {
-      gatsbyImageData(
-        quality: 100
-        width: 250
-        placeholder: TRACED_SVG
-        layout: CONSTRAINED
-      )
-    }
-  }
-  mike: file(relativePath: {eq: "mike-min.jpg"}) {
-    childImageSharp {
-      gatsbyImageData(
-        quality: 100
-        width: 250
-        placeholder: TRACED_SVG
-        layout: CONSTRAINED
-      )
-    }
-  }
-  caleb: file(relativePath: {eq: "caleb-min.jpg"}) {
-    childImageSharp {
-      gatsbyImageData(
-        quality: 100
-        width: 250
-        placeholder: TRACED_SVG
-        layout: CONSTRAINED
-      )
-    }
-  }
-  keith: file(relativePath: {eq: "keith.jpg"}) {
-    childImageSharp {
-      gatsbyImageData(
-        quality: 100
-        width: 250
-        placeholder: TRACED_SVG
-        layout: CONSTRAINED
-      )
-    }
-  }
-}
-`);
+  `);
 
   return (
-    <section className='bg-white w-full flex items-center justify-center'>
-      <article className='testimonials-container w-full items-center p-12 md:gap-4'>
-        <div className='testimonials-container-1 sm:text-sm md:text-base lg:text-lg sm:mb-10 md:mb-8 lg:mb-0'>
-          <h3 className='text-myPurple font-bold sm:text-lg md:text-xl lg:text-2xl xl:text-3xl'>
-            Here Are Just A Few of the Great Companies & People I&apos;ve Helped
+    <section className="bg-white w-full flex items-center justify-center">
+      <article className="testimonials-container w-full items-center p-12 md:gap-4">
+        <div className="testimonials-container-1 sm:text-sm md:text-base lg:text-lg sm:mb-6 md:mb-4 lg:mb-0">
+          <h3 className="text-myPurple font-bold sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">
+            Some of the Great Companies & People I&apos;ve Helped
+
+            THIS SECTION NEEDS A LOT OF WORK
           </h3>
-          <p className='pt-4 sm:text-base md:text-xl'>
-            If you have a project or an idea, I&apos;d love to work with you to bring it to life. My passon is helping
-            people and small businesses get up and running with a fast, reliable, customer focused website.
-          </p>
-          <p className='pt-4 sm:text-base md:text-xl'>Let me put that passion to work for you.</p>
-          <div className='testimonials-container-link-container transform hover:scale-105 transition-all duration-700 ease-in-out mt-6'>
-            <a
-              href='#hire-me'
-              className='shadow-lg pt-3 pl-4 pr-4 pb-2 rounded-sm sm:text-base md:text-xl text-lg shadow text-white hover:underline bg-myRed hover:bg-myDarkRed'>
-              Get Started Today!
-            </a>
-          </div>
         </div>
-        <div className='testimonials-container-2'>
+        <div className="testimonials-container-2">
           {testimonials.map((card, index) => (
             <div
               key={index}
               data-key={index + 1}
-              className='customer-testimonial-container flex sm:p-2 md:p-4 lg:p-6 items-center md:justify-center lg:justify-around max-w-2xl sm:text-sm md:text-base'>
-                {/* //! Need to set the alt for each of these !// */}
+              className="customer-testimonial-container flex sm:p-2 md:p-4 lg:p-6 items-center md:justify-center lg:justify-around max-w-3xl sm:text-sm md:text-base"
+            >
+              {/* //! Need to set the alt for each of these !// */}
               <GatsbyImage
-                image={index + 1 === 1
-                  ? query.stu.childImageSharp.gatsbyImageData
-                  : index + 1 === 2
+                image={
+                  index + 1 === 1
+                    ? query.stu.childImageSharp.gatsbyImageData
+                    : index + 1 === 2
                     ? query.tracy.childImageSharp.gatsbyImageData
                     : index + 1 === 3
-                      ? query.mike.childImageSharp.gatsbyImageData
-                      : index + 1 === 4
-                        ? query.caleb.childImageSharp.gatsbyImageData
-                        : index + 1 === 5
-                          ? query.keith.childImageSharp.gatsbyImageData
-                          : ''}
-                className='testimonials-images rounded-sm shadow-lg border-2 border-solid border-black-100' 
-                alt=''/>
-              <div className='pl-4 sm:text-lg md:text-xl'>
-                <h4 className='lg:text-2xl text-myPurple font-bold'>{card.name}</h4>
-                <p className='text-myBlack hidden md:inline'>{card.title} - </p>
-                <p className='text-myBlack hidden md:inline'>{card.company}</p>
-                <p className='sm:max-w-full md:max-w-sm lg:max-w-lg italic'>{card.comment}</p>
+                    ? query.mike.childImageSharp.gatsbyImageData
+                    : index + 1 === 4
+                    ? query.caleb.childImageSharp.gatsbyImageData
+                    : index + 1 === 5
+                    ? query.keith.childImageSharp.gatsbyImageData
+                    : ""
+                }
+                className="testimonials-images rounded-sm shadow-lg border-2 border-solid border-black-100"
+                alt=""
+              />
+              <div className="pl-4 sm:text-lg md:text-xl">
+                <h4 className="lg:text-2xl text-myPurple font-bold">{card.name}</h4>
+                <p className="text-myBlack hidden md:inline">{card.title} - </p>
+                <p className="text-myBlack hidden md:inline">{card.company}</p>
+                <p className="sm:max-w-full md:max-w-md lg:max-w-lg italic">{card.comment}</p>
               </div>
             </div>
           ))}
+        </div>
+        <div className="testimonials-container-3">
+          <p className="pt-4 sm:text-base md:text-xl">
+            If you have a project or an idea, I&apos;d love to work with you to bring it to life.
+          </p>
+          <p className="pt-4 sm:text-base md:text-xl">Let me put that passion to work for you.</p>
+          <div className="testimonials-container-link-container transform hover:scale-105 transition-all duration-700 ease-in-out mt-6">
+            <a
+              href="#hire-me"
+              className="shadow-lg pt-3 pl-4 pr-4 pb-2 rounded-sm sm:text-base md:text-xl text-lg shadow text-white hover:underline bg-myRed hover:bg-myDarkRed"
+            >
+              Get Started Today!
+            </a>
+          </div>
         </div>
       </article>
     </section>
