0a4dc3f3188258e77ab2dc6c133a6019bfe65bb3
Jonathan Sexton <joncsexton@.com>
2021-05-06 13:00:58 -0500

    changes to header styling

--- v2/src/components/header.js
+++ v2/src/components/header.js
@@ -1,46 +1,50 @@
-import { graphql, useStaticQuery, Link } from 'gatsby';
-import React, { useState } from 'react';
+import { graphql, useStaticQuery, Link } from "gatsby";
+import React, { useState } from "react";
 import { GatsbyImage } from "gatsby-plugin-image";
 
 function Header() {
   const [isExpanded, toggleExpansion] = useState(false);
-  let [hovered, setHovered] = useState(false);
-  const toggleHover = () => setHovered(true);
-  const toggleHover2 = () => setHovered(false);
-  const queryData = useStaticQuery(graphql`query SiteTitleQuery {
-  site {
-    siteMetadata {
-      title
+  // let [hovered, setHovered] = useState(false);
+  // const toggleHover = () => setHovered(true);
+  // const toggleHover2 = () => setHovered(false);
+  const queryData = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+      brand: file(relativePath: { eq: "icons/Brand-min.png" }) {
+        childImageSharp {
+          gatsbyImageData(quality: 100, width: 200, layout: CONSTRAINED)
+        }
+      }
     }
-  }
-  brand: file(relativePath: {eq: "icons/Brand-min.png"}) {
-    childImageSharp {
-      gatsbyImageData(quality: 100, width: 200, layout: CONSTRAINED)
-    }
-  }
-}
-`);
+  `);
 
+  // className={hovered ? 'logoNotHovered' : 'logo'} />
   return (
-    <header className='bg-myPurple text-white z-20'>
-      <div className='flex flex-wrap items-center justify-between md:justify-center lg:justify-evenly max-w-6xl lg:max-w-full'>
-        <span className='flex items-center'>
-          <Link to='/' onMouseLeave={toggleHover} onMouseEnter={toggleHover2}>
+    <header className="bg-myPurple text-white">
+      <div className="flex flex-wrap items-center justify-between md:justify-center lg:justify-evenly max-w-6xl lg:max-w-full">
+        <span className="flex items-center">
+          <Link to="/">
             <GatsbyImage
               image={queryData.brand.childImageSharp.gatsbyImageData}
               alt="the letters JS on a space background"
-              className={hovered ? 'logoNotHovered' : 'logo'} />
+              className="logo"
+            />
           </Link>
-          <p className='hidden md:inline mb-2 md:mb-4 lg:mb-6 sm:lg md:text-xl lg:text-2xl'>Jonathan Sexton</p>
+          <p className="hidden md:inline mb-2 md:mb-4 lg:mb-6 sm:lg md:text-xl lg:text-2xl">Jonathan Sexton</p>
         </span>
 
         <button
-          type='button'
-          className='items-center block px-3 py-2 border-white rounded-sm md:hidden'
-          onClick={() => toggleExpansion(!isExpanded)}>
-          <svg className='w-8 h-8 fill-current' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'>
+          type="button"
+          className="items-center block px-3 py-2 border-white rounded-sm md:hidden"
+          onClick={() => toggleExpansion(!isExpanded)}
+        >
+          <svg className="w-8 h-8 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
             <title>Menu</title>
-            <path d='M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z' />
+            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
           </svg>
         </button>
 
@@ -77,12 +81,12 @@ function Header() {
               classname: `pt-2 pr-2 pl-2 pb-1 rounded-sm sm:lg md:text-xl lg:text-2xl hover:text-myRed ml-4 bg-myRed hover:bg-white hover:underline`,
             },
           ].map((link) =>
-            link.route === 'https://jonathansexton.me/blog' ? (
-              <a href='https://jonathansexton.me/blog' className={link.classname} key={link.title}>
+            link.route === "https://jonathansexton.me/blog" ? (
+              <a href="https://jonathansexton.me/blog" className={link.classname} key={link.title}>
                 Blog
               </a>
             ) : (
-              <Link className={link.classname} key={link.title} to={link.route} rel='noopener noreferrer'>
+              <Link className={link.classname} key={link.title} to={link.route} rel="noopener noreferrer">
                 {link.title}
               </Link>
             )
