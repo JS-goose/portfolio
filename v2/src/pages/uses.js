d877716787f7cf46ffa9803d7173d06a0c5bb2d7
Jonathan Sexton <joncsexton@gmail.com>
2021-05-18 22:10:15 -0500

    generated basic uses page

---
+++ v2/src/pages/uses.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import SEO from '../components/seo';
+
+const Uses = () => (
+    <div>
+        <SEO
+            keywords={[
+                `freelance`,
+                `team`,
+                `jonathan`,
+                `sexton`,
+                `development`,
+                `website`,
+                `technology`,
+                `what i use`
+            ]}
+            title="Uses"
+        />
+        <div>
+            <p>Here is a list of the tech I currently use</p>
+        </div>
+    </div>
+)
+
+export default Uses;
