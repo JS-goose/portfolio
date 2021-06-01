376030b6bf7b138fffcfc0c790044102f8797a88
Jonathan Sexton <joncsexton@.com>
2021-06-01 12:57:22 -0500

    services changes

--- v2/src/components/servicesList.js
+++ v2/src/components/servicesList.js
@@ -1,32 +1,32 @@
-import React from 'react';
-import { graphql, useStaticQuery, Link } from 'gatsby';
+import React from "react";
+import { graphql, useStaticQuery, Link } from "gatsby";
 
 const services = [
-  // {
-  //   number: `1`,
-  //  title: '',
-  //   desc: 'Provide a' +
-  //       `${<Link to="/consulting">consulting</Link>}` +
-  //       'to discuss any pain points you have with your web presence (or lack of).',
-  // },
+  {
+    number: `1`,
+    title: "Website Audit",
+    desc:
+      "A thorough review of your current website to identify any performance/user experience issues that might prevent you from converting that visitor into a customer and then propose solutions to those potential roadblocks",
+    imgAlt: "two people having a great conversation with chat bubbles above their heads",
+  },
   // {
   //   number: `2`,
   //   title: '',
   //   desc: `Assess your current website to identify issues, propose solutions, and ultimately correct those issues.`,
   // },
   {
-    number: `3`,
-    title: 'Website Design',
+    number: `2`,
+    title: "Website Design",
     desc: `Design a custom website from start to finish that you sign off on before any build work is started.`,
   },
   {
-    number: `4`,
-    title: 'Personalized Development',
+    number: `3`,
+    title: "Personalized Development",
     desc: `Tailor that solution with ease of use in mind to draw in more customers and expand your presence.`,
   },
   {
-    number: `5`,
-    title: 'Marketing Solutions',
+    number: `4`,
+    title: "Marketing Solutions",
     desc: `Ensure SEO is at the forefront of the work I do so you're website reaches your audience`,
   },
   // {
@@ -42,7 +42,12 @@ const services = [
   //   number: `9`,
   //   desc: `Provide updates, changes, edits, or technical overhauls to your website via reoccuring service subscription.`,
   // },
-  { number: `10`, title: 'Continued Support', desc: `Guarantee my work for a minimum of 6 months after the launch of the project.` },
+  {
+    number: `5`,
+    title: "Content Solutions",
+    desc: `Filling your website with content can be laborious - especially if it's written content like articles.  Let me handle that for you!`,
+  },
+  { number: `6`, title: "Continued Support", desc: `Guarantee my work for a minimum of 6 months after the launch of the project.` },
 ];
 
 const ServicesList = () => {
@@ -60,22 +65,42 @@ const ServicesList = () => {
         extension
         publicURL
       }
+      design: file(relativePath: { eq: "icons/design.svg" }) {
+        extension
+        publicURL
+      }
+      development: file(relativePath: { eq: "icons/development.svg" }) {
+        extension
+        publicURL
+      }
+      marketing: file(relativePath: { eq: "icons/marketing.svg" }) {
+        extension
+        publicURL
+      }
+      content: file(relativePath: { eq: "icons/content.svg" }) {
+        extension
+        publicURL
+      }
+      support: file(relativePath: { eq: "icons/support.svg" }) {
+        extension
+        publicURL
+      }
     }
   `);
 
   return (
-    <section className='flex flex-col mt-16 mb-16 bg-white grid-row-3 items-center w-full'>
-      <article className='services-list-container text-lg items-center'>
-        <div className='services-list'>
-          <h1 id="services" className='md:pb-6 lg:pb-12 sm:text-2xl lg:text-3xl xl:text-4xl'>
+    <section className="flex flex-col mt-16 mb-16 bg-white grid-row-3 items-center w-full">
+      <article className="services-list-container text-lg items-center">
+        <div className="services-list">
+          <h1 id="services" className="md:pb-6 lg:pb-12 sm:text-2xl lg:text-3xl xl:text-4xl">
             What Can I Do For You and Your Business?
           </h1>
           <div>
             {/* Image courtesy of https://undraw.co */}
-            <img src={query.decisions.publicURL} alt='a man in a suit pointing at charts' />
+            <img src={query.decisions.publicURL} alt="a man in a suit pointing at charts" />
           </div>
-          <ul className="mt-8">
-            <li className='pb-4 sm:text-base md:text-lg lg:text-xl'>
+          <div className="flex flex-row flex-wrap w-100 mt-8">
+            {/* <li className='pb-4 sm:text-base md:text-lg lg:text-xl'>
               <article className="flex flex-col">
                 <div className="flex items-center p-1 w-100">
                   <img src={query.meeting.publicURL} alt='two people having a great conversation with chat bubbles above their heads' />
@@ -89,63 +114,68 @@ const ServicesList = () => {
                   &nbsp;to discuss any pain points you have with your current (or lack of) web presence
                  </div>
               </article>
-            </li>
+            </li> */}
             {services.map((service) => (
-              <li key={service.number} className='pb-4 sm:text-base md:text-lg lg:text-xl'>
-                {service.number}. {service.desc}
-              </li>
+              <article key={service.number} className="pb-4 sm:text-base md:text-lg lg:text-xl">
+                <div className="flex flex-col">
+                  <div className="flex p-1 w-100">
+                    <img
+                      src={service.number === "1" ? query.meeting.publicURL : 
+                      service.number === "2" ? query.design.publicURL : 
+                      service.number === "3" ? query.development.publicURL : 
+                      service.number === "4" ? query.marketing.publicURL :
+                      service.number === "5" ? query.content.publicURL :
+                      service.number === "6" ? query.support.publicURL : ""}
+                      alt={service.imgAlt}
+                    />
+                    <p className="pl-4 text-2xl">{service.title}</p>
+                  </div>
+                  <div className="max-w-md p-1">{service.desc}</div>
+                </div>
+              </article>
             ))}
-          </ul>
-          <p className="pt-6 sm:text-base md:text-lg lg:text-xl">Need more information about my services?  Get in touch today by using the <a href="#hire-me">contact form</a>!</p>
+          </div>
+          <p className="pt-6 sm:text-base md:text-lg lg:text-xl">
+            Need more information about my services? Get in touch today by using the <a href="#hire-me">contact form</a>!
+          </p>
         </div>
       </article>
-      <article className='why-hire-me-container items-center sm:text-base md:text-lg lg:text-xl'>
+      <article className="why-hire-me-container items-center sm:text-base md:text-lg lg:text-xl">
         <div>
-          <h1 className='md:pb-6 lg:pb-12 sm:text-2xl lg:text-3xl xl:text-4xl'>
-            Why Hire Me As Your Developer?
-          </h1>
+          <h1 className="md:pb-6 lg:pb-12 sm:text-2xl lg:text-3xl xl:text-4xl">Why Hire Me As Your Developer?</h1>
           <div className="mb-8">
             {/* Image courtesy of https://undraw.co */}
-            <img
-              src={query.shop.publicURL}
-              alt='a man standing outside a shop of books'
-              className='why-hire-me-container-books-img'
-            />
+            <img src={query.shop.publicURL} alt="a man standing outside a shop of books" className="why-hire-me-container-books-img" />
           </div>
           <p>
-            Having a digital presence has never been more important than it is now. With consumers shifting more activities
-            online you could be missing out on customers and profits if you don&apos; have an online
-            presence.
+            Having a digital presence has never been more important than it is now. With consumers shifting more activities online you
+            could be missing out on customers and profits if you don&apos; have an online presence.
           </p>
           &nbsp;
           <p>
-            By partnering with me, you&apos;ll get a responsive, easy to use, functional, SEO optimized website
-            that will ensure a great user experience.
+            By partnering with me, you&apos;ll get a responsive, easy to use, functional, SEO optimized website that will ensure a
+            great user experience.
           </p>
           &nbsp;
           <p>
-            From sprawling, complex web applications to custom landing pages and sales funnels I&apos;ll put my industry
-            knowledge and experience to work for you.
+            From sprawling, complex web applications to custom landing pages and sales funnels I&apos;ll put my industry knowledge and
+            experience to work for you.
           </p>
           &nbsp;
           <p>
-            I provide big agency level service and support with the same care and attention you get from local, hometown
-            businesses.
+            I provide big agency level service and support with the same care and attention you get from local, hometown businesses.
           </p>
           &nbsp;
           <p>
-            I communicate well and often in an easily understanable way. I&apos;m comfortable working with your current
-            team or in a solo scenario; I&apos;m independent and trustworthy giving you time to do what matters - focus
-            on your business.
+            I communicate well and often in an easily understanable way. I&apos;m comfortable working with your current team or in a
+            solo scenario; I&apos;m independent and trustworthy giving you time to do what matters - focus on your business.
           </p>
           &nbsp;
-          <p>
-            This is a partnership and I want you to be a part of the process.
-          </p>
+          <p>This is a partnership and I want you to be a part of the process.</p>
           &nbsp;
           <p>
-            From
-            the initial meeting, through the design and build process, all the way to launching your website we&apos;ll be there together.
+            From the initial meeting, through the design and build process, all the way to launching your website we&apos;ll be there
+            together.
           </p>
         </div>
       </article>
