be4709ab15db8a92004c884368092b2f8fff27de
Jonathan Sexton <joncsexton@.com>
2021-06-08 12:34:55 -0500

    form picture

--- v2/src/components/getStarted.js
+++ v2/src/components/getStarted.js
@@ -6,7 +6,7 @@ const GetStarted = () => {
     <section
       className='get-started-section-container md:p-2 flex flex-col items-center sm:text-base md:text-lg lg:text-xl'
       id='hire-me'>
-        <ContactForm formTitle={`Let's get started on your project today!`} bgColor={`bg-white`}/>
+        <ContactForm formTitle={`Let's get started on your project today!`} bgColor={`white`}/>
       </section>
   );
 };
