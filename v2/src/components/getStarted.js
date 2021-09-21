aecfc5fe03331f3a25d63d463b9a2dd7d613c3ce
Jonathan Sexton <joncsexton@gmail.com>
2021-09-21 15:18:09 -0500

    added form title back

--- v2/src/components/getStarted.js
+++ v2/src/components/getStarted.js
@@ -6,7 +6,7 @@ const GetStarted = () => {
     <section
       className='get-started-section-container md:p-2 flex flex-col items-center sm:text-base md:text-lg lg:text-xl'
       id='hire-me'>
-        <ContactForm bgColor={`white`}/>
+        <ContactForm bgColor={`white`} formTitle={`Let's get started on your project today!`}/>
       </section>
   );
 };
