53e6e7c1793c39e4ad8f2e29848215f9d8d93099
Jonathan Sexton <joncsexton@gmail.com>
2021-09-22 05:57:42 -0500

    copy spacing changes

--- v2/src/components/cta.js
+++ v2/src/components/cta.js
@@ -25,20 +25,20 @@ const CallToAction = () => {
       <article className='cta-img-container sm:p-2 lg:p-6'>
         <GatsbyImage image={query.me.childImageSharp.gatsbyImageData} alt="Jonathan Sexton pretending to write something clever in his journal while sitting at a desk" className='me-img' />
         <div className='flex flex-row justify-center sm:pt-2'>
-          <h5 className='md:text-base lg:text-lg font-Rubik'>Hi, I&apos;m Jonathan :)</h5>
+          <p className='md:text-base lg:text-lg'>Hi, I&apos;m Jonathan :)</p>
         </div>
       </article>
       <span className='hidden md:visible border border-right border-myRed inline-block mt-6 mb-8 ml-1 mr-4' />
       <article className='p-3 sm:text-sm md:text-base lg:text-lg'>
         <div className='flex flex-col justify-evenly max-w-3xl h-full'>
-          <h1 className='sm:text-lg md:text-2xl lg:text-3xl xl:text-5xl underline'>
+          <h1 className='sm:text-xl md:text-2xl lg:text-3xl xl:text-5xl underline'>
             I build blazing fast websites that help small businesses like yours convert visitors into customers!
           </h1>
-          <p>
+          <p className="py-6 sm:text-base md:text-lg lg:text-xl">
             I&apos;m a freelance developer specializing in helping people and businesses expand their online presence,
             reach more customers, and increase conversion rates!
           </p>
-          <p>
+          <p className="pb-6 sm:text-base md:text-lg lg:text-xl">
             Are you looking for a developer to design, build, and publish your website?  Do you need an audit of your existing website? You&apos;ve
             come to the right place then my friend!
           </p>
