7aae55c9a78c06cbc7a241398edb60b741ecbc72
Jonathan Sexton <joncsexton@gmail.com>
2021-09-22 05:56:43 -0500

    font family update

--- v2/gatsby-config.js
+++ v2/gatsby-config.js
@@ -14,7 +14,7 @@ module.exports = {
       resolve: 'gatsby-plugin-web-font-loader',
       options: {
         google: {
-          families: ['Open Sans', 'Roboto', 'Kumbh Sans', 'Rubik'],
+          families: ['Open Sans'],
         },
       },
     },
