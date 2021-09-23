6d5f7747616a48a97cea6e8877da1fa982048fca
Jonathan Sexton <joncsexton@gmail.com>
2021-09-23 05:20:05 -0500

    added home button

--- v2/src/pages/contactsent.js
+++ v2/src/pages/contactsent.js
@@ -2,7 +2,7 @@
 /* eslint-disable import/namespace */
 import React from 'react';
 import SEO from '../components/seo';
-import { graphql, useStaticQuery } from 'gatsby';
+import { graphql, useStaticQuery, Link } from 'gatsby';
 
 export default function ContactSent() {
     const query = useStaticQuery(graphql`{
@@ -12,28 +12,35 @@ export default function ContactSent() {
         }
     }`)
     return (
-       <section className="flex flex-col min-w-full max-w-screen items-center text-white h-screen">
+        <section className="flex flex-col min-w-full max-w-screen items-center text-white h-screen">
             <SEO
-            keywords={[
-                `boerne web developer`,
-                `web developer`,
-                `web developer in boerne, tx`,
-                `small business website`,
-                `freelance developer`,
-                `build website for tile laying`,
-                `freelance web developer`,
-                `jonathan`,
-                `jonathan sexton`,
-                `consulting`,
-            ]}
-            title="Consulting"
-        />
-        <article className="h-full p-6 sm:text-base md:text-lg lg:text-xl flex flex-col justify-evenly">
-            <h1 className="md:pb-6 lg:pb-10 md:pt-6 lg:pt-6 sm:text-2xl lg:text-3xl xl:text-4xl">Thank you!</h1>
-            <p className="pb-6 sm:text-base md:text-lg lg:text-xl">Your email has been sent.  I respond to all contact within 24 hours, so keep an eye on your inbox!</p>
-            <img src={query.email.publicURL} alt="a person leaning against a huge envelop with a green checkmark on it" />
-        </article>
-       </section>
+                description="The page displayed to a user after sending an email to Jonathan Sexton"
+                lang="en"
+                title="Contact Sent"
+                keywords={[
+                    `boerne web developer`,
+                    `web developer`,
+                    `web developer in boerne, tx`,
+                    `small business website`,
+                    `freelance developer`,
+                    `build website for tile laying`,
+                    `freelance web developer`,
+                    `jonathan`,
+                    `jonathan sexton`,
+                    `consulting`,
+                ]}
+            />
+            <article className="h-full p-6 sm:text-base md:text-lg lg:text-xl flex flex-col justify-evenly">
+                <h1 className="pb-2 sm:pb-4 md:pb-6 lg:pb-10 md:pt-6 lg:pt-6 sm:text-2xl lg:text-3xl xl:text-4xl">Thank you!</h1>
+                <p className="pb-2 sm:pb-4 md:pb-6 lg:pb-8  sm:text-base md:text-lg lg:text-xl">Your email has been sent.  I respond to all contact within 24 hours, so keep an eye on your inbox!</p>
+                <div className="flex justify-center">
+                    <button type="button" className="shadow-lg bg-myDarkRed p-2 rounded transform hover:scale-105 transition-all duration-700 ease-in-out hover:underline hover:bg-myRed">
+                        <Link to="/">Take Me Home</Link>
+                    </button>
+                </div>
+                <img src={query.email.publicURL} alt="a person leaning against a huge envelop with a green checkmark on it" />
+            </article>
+        </section>
     )
 }
 
